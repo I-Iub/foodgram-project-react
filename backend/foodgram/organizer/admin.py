870e64841bd843a097c0641aa5ffbf59f396e84b
rashid <ri-post@yandex.ru>
2021-11-08 22:52:23 +0500

    Зарегистрировал модели в админке для organizer

--- backend/foodgram/organizer/admin.py
+++ backend/foodgram/organizer/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Favorite, ShoppingCart, Subscription
 
-# Register your models here.
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'recipe')
+    search_fields = ('user',)
+    list_filter = ('user',)
+    empty_value_display = '-пусто-'
+
+
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user')
+    search_fields = ('user',)
+    list_filter = ('user',)
+    empty_value_display = '-пусто-'
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'author')
+    search_fields = ('user',)
+    list_filter = ('user',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Favorite, FavoriteAdmin)
+admin.site.register(ShoppingCart, ShoppingCartAdmin)
+admin.site.register(Subscription, SubscriptionAdmin)
--- backend/foodgram/organizer/models.py
+++ backend/foodgram/organizer/models.py
@@ -12,8 +12,16 @@ class ShoppingCart(models.Model):
     )
     recipe = models.ManyToManyField(
         Recipe,
-        related_name='cart'
+        related_name='shopping_cart'
     )
+
+    class Meta:
+        verbose_name = 'Список покупок'
+        verbose_name_plural = 'Списки покупок'
+
+    def __str__(self):
+        return (f'Список покупок пользователя {self.user.first_name}'
+                f'{self.user.last_name}')
 
 
 class Subscription(models.Model):
@@ -28,6 +36,20 @@ class Subscription(models.Model):
         related_name='authors'
     )
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='unique_subscription'
+            ),
+            models.CheckConstraint(
+                check=~models.Q(
+                    author=models.F('user')), name='user_is_not_author'
+                )
+        ]
+        verbose_name = 'Подписка',
+        verbose_name_plural = 'Подписки'
+
 
 class Favorite(models.Model):
     user = models.ForeignKey(
@@ -38,3 +60,10 @@ class Favorite(models.Model):
         Recipe,
         on_delete=models.PROTECT
     )
+
+    class Meta:
+        verbose_name = 'Избранное'
+        verbose_name_plural = 'Избранное'
+
+    def __str__(self):
+        return f'Избранное пользователя {self.user.first_name} {self.user.last_name}'
