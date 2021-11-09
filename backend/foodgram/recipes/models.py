8b48a9ff0e8368fa80f2763a627c06957168581c
rashid <ri-post@yandex.ru>
2021-11-09 21:56:23 +0500

    Бестолковый коммит

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,10 +1,5 @@
-from datetime import timedelta
-
 from django.db import models
 from users.models import User
-
-RECIPE_MIN_COOKING_TIME = timedelta(minutes=1)
-INGREDIENT_MIN_AMOUNT = 0
 
 
 class Recipe(models.Model):
@@ -28,21 +23,13 @@ class Recipe(models.Model):
     name = models.CharField(max_length=200, verbose_name='Название блюда')
     image = models.ImageField()
     text = models.TextField(verbose_name='Рецепт')
-    cooking_time = models.DurationField(
-        verbose_name='Время приготовления, мин.',
-        default=RECIPE_MIN_COOKING_TIME
+    cooking_time = models.DurationField(  # сделать валидацию (>= 1)
+        verbose_name='Время приготовления, мин.'
     )
 
     class Meta:
-        constraints = [
-            models.CheckConstraint(
-                check=models.Q(cooking_time__gte=RECIPE_MIN_COOKING_TIME),
-                name=f'cooking_time__gte_{str(RECIPE_MIN_COOKING_TIME)}_minute'
-            )
-        ]
-
-    def __str__(self):
-        return self.name[:20]
+        def __str__(self):
+            return self.name[:20]
 
 
 class Tag(models.Model):
@@ -50,8 +37,9 @@ class Tag(models.Model):
     color = models.ImageField(unique=True, max_length=7)
     slug = models.SlugField(unique=True, max_length=200)
 
-    def __str__(self):
-        return self.name
+    class Meta:
+        def __str__(self):
+            return self.name
 
 
 class Ingredient(models.Model):
@@ -61,28 +49,23 @@ class Ingredient(models.Model):
         related_name='ingredients',
         verbose_name='Ингредиент'
     )
-    amount = models.DecimalField(
+    amount = models.DecimalField(  # сделать валидацию > 0
         max_digits=9,
         decimal_places=3,
         verbose_name='Количество'
     )
 
     class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=['measurement_unit', 'amount'],
-                name='unique_ingredient_amount'
-            ),
-            models.CheckConstraint(
-                check=models.Q(amount__gt=INGREDIENT_MIN_AMOUNT),
-                name=f'amount__gt_{INGREDIENT_MIN_AMOUNT}'
+        constraints = [models.UniqueConstraint(
+            fields=['measurement_unit', 'amount'],
+            name='unique_ingredient_amount'
             )
         ]
         verbose_name = 'Ингредиент'
         verbose_name_plural = 'Ингредиенты'
 
-    def __str__(self):
-        return self.measurement_unit
+        def __str__(self):
+            return self.measurement_unit
 
 
 class Measurement(models.Model):
@@ -96,10 +79,9 @@ class Measurement(models.Model):
     )
 
     class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=['name', 'measure'],
-                name='unique_measurement'
+        constraints = [models.UniqueConstraint(
+            fields=['name', 'measure'],
+            name='unique_measurement'
             )
         ]
         verbose_name = 'Компонент',
