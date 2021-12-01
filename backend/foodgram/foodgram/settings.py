cd57ef552bfdb5d6b9d88aae2888adae3618d39f
rashid <ri-post@yandex.ru>
2021-12-02 01:05:18 +0500

    Переименовал папку static в static_foodgram

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -139,9 +139,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_foodgram/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_foodgram')
 
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
